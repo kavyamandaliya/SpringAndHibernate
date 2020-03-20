082342db3441d59e4484dafd321caa91f2138dd5
Kavya Mandaliya <kavya@kavyas-air.lan>
2020-03-20 11:22:04 -0700

    bean scopes

--- spring-in-5-steps/src/main/java/com/learning/spring/basics/springin5steps/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/learning/spring/basics/springin5steps/BinarySearchImpl.java
@@ -1,14 +1,20 @@
 package com.learning.spring.basics.springin5steps;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl {
 	//sorting an array 
 	//search the array 
 	//return the result index
+	
 	@Autowired
+	@Qualifier("quick")
 	private SortAlgorithm sortAlgorithm;
 	
 	public int binarySearch(int[] numbers, int target) {		
