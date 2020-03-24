55b2cb92368ee8ee4fa73fbdcbfc9d3a9b656df5
Kavya Mandaliya <kavya.mandaliya@sjsu.edu>
2020-03-23 23:57:46 -0700

    xml

--- spring-in-5-steps/src/main/java/com/learning/spring/basics/springin5steps/SpringIn5StepsXMLContextApplication.java
+++ spring-in-5-steps/src/main/java/com/learning/spring/basics/springin5steps/SpringIn5StepsXMLContextApplication.java
@@ -1,5 +1,7 @@
 package com.learning.spring.basics.springin5steps;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,10 +12,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.learning.spring.basics.springin5steps.basics.BinarySearchImpl;
 import com.learning.spring.basics.springin5steps.xml.XMLPersonDAO;
 
-@Configuration
-@ComponentScan("com.learning.spring.basics.springin5steps")
-public class SpringIn5StepsXMLContextApplication {
 
+public class SpringIn5StepsXMLContextApplication {
+	private static Logger LOGGER = 
+			LoggerFactory.getLogger(SpringIn5StepsScopeApplication.class);
 	//what are beans 
 	//what are dependencies for the bean 
 	//where to search for the beans 
@@ -22,7 +24,8 @@ public class SpringIn5StepsXMLContextApplication {
 		//application context maintains all the beans 
 		ClassPathXmlApplicationContext applicationContext = 
 				new ClassPathXmlApplicationContext("ApplicationContext.xml");
-	
+		LOGGER.info("beans-> {}", 
+				(Object)applicationContext.getBeanDefinitionNames());
 		XMLPersonDAO person = applicationContext.getBean(XMLPersonDAO.class);
 		
 		System.out.println(person);
