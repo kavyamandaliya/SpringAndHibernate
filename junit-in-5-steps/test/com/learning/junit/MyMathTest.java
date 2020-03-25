35bd3013090b6e035c279ce8417af0fc1e81e430
Kavya Mandaliya <kavya.mandaliya@sjsu.edu>
2020-03-25 11:06:53 -0700

    started junit

---
+++ junit-in-5-steps/test/com/learning/junit/MyMathTest.java
@@ -0,0 +1,15 @@
+package com.learning.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyMathTest {
+
+	@Test
+	void test() {
+		//absence of failure is success 
+		
+	}
+
+}
