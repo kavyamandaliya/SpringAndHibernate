e7c562fd4a3b91b9d8d17ececf389fc54ffa03b1
Kavya Mandaliya <kavya.mandaliya@sjsu.edu>
2020-03-25 11:47:13 -0700

    several asserts

---
+++ junit-in-5-steps/test/com/learning/junit/AssertTest.java
@@ -0,0 +1,17 @@
+package com.learning.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AssertTest {
+
+	@Test
+	void test() {
+		//assertEquals(1,1);
+		//assertEquals(true, true);
+		assertFalse(false);
+		
+	}
+
+}
--- junit-in-5-steps/test/com/learning/junit/MyMathTest.java
+++ junit-in-5-steps/test/com/learning/junit/MyMathTest.java
@@ -5,10 +5,32 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
-
+	MyMath myMath = new MyMath();
 	@Test
-	void test() {
+	void sum1() {
 		//absence of failure is success 
+		
+		int result = myMath.sum(new int[] {1,2,3});
+//		System.out.println(result);
+		assertEquals(6, result); 
+		
+	}
+	
+	@Test
+	void sum2() {
+		//absence of failure is success 
+		int result = myMath.sum(new int[] {3});
+//		System.out.println(result);
+		assertEquals(3, result); 
+		
+	}
+	
+	@Test
+	void sum3() {
+		//absence of failure is success 
+		int result = myMath.sum(new int[] {});
+//		System.out.println(result);
+		assertEquals(0, result); 
 		
 	}
 
